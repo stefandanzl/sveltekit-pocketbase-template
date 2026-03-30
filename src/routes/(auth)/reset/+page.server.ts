9ead8dd02d48a6458eeb05c3478f0afd78e331a6
Stefan Danzl <stefan.danzl@live.de>
2026-03-30 03:17:28 +0200

    remove .js

--- src/routes/(auth)/reset/+page.server.ts
+++ src/routes/(auth)/reset/+page.server.ts
@@ -1,17 +1,17 @@
-import { pbError } from '$lib/pocketbase.svelte.js'
+import { pbError } from '$lib/pocketbase.svelte';
 
 export const actions = {
 	default: async ({ locals, request }) => {
 		const form = Object.fromEntries(await request.formData()) as {
-			email: string
-		}
+			email: string;
+		};
 
 		try {
-			const result = await locals.pb.collection('users').requestPasswordReset(form.email)
-			return { success: result, email: form.email }
+			const result = await locals.pb.collection('users').requestPasswordReset(form.email);
+			return { success: result, email: form.email };
 		} catch (e) {
-			console.error(e)
-			pbError(e)
+			console.error(e);
+			pbError(e);
 		}
-	},
-}
+	}
+};
