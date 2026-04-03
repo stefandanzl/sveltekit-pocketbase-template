8f0e83ec410fb386dedfa69c42f9bbbe3416633d
Stefan Danzl <19376127+stefandanzl@users.noreply.github.com>
2026-04-03 03:24:19 +0200

    404 error created by this file!

--- src/hooks.ts
+++
@@ -1,8 +0,0 @@
-import type { Reroute } from '@sveltejs/kit'
-
-export const reroute: Reroute = ({ url }) => {
-	if (url.hostname !== 'localhost') {
-		return '/' + url.hostname + url.pathname
-	}
-	return url.pathname
-}
